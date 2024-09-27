c741e938e5a7772243edcd4f104cf2b38b96fa2d
ANZAR <anzarkhan790@gmail.com>
2024-09-27 18:33:06 +0530

    success/cancel url update

--- src/actions/index.js
+++ src/actions/index.js
@@ -203,8 +203,8 @@ export async function createStripePaymentAction(data) {
     payment_method_types: ["card"],
     line_items: data?.lineItems,
     mode: "subscription",
-    success_url: "http://localhost:3000/membership?status=success",
-    cancel_url: "http://localhost:3000/membership?status=cancel",
+    success_url: `${process.env.URL}/membership` + "?status=success",
+    cancel_url: `${process.env.URL}/membership` + "?status=cancel",
 
     customer: customerId, // Pass the customer ID only
     billing_address_collection: "required", // Required for Indian regulations
